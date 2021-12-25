bc141a87af88f584f9cc61ae029a53cf87e27a1b
parallelqw <privatelle@tutanota.com>
2021-12-25 19:36:37 +0300

    Updated tags.

--- dwm-6.2/config.h
+++ dwm-6.2/config.h
@@ -11,8 +11,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "TerminessTTF Nerd Font Mono:size=13" }; /* Ubuntu Nerd Font*/
-static const char dmenufont[]       = "TerminessTTF Nerd Font Mono:size=13";
+static const char *fonts[]          = { "TerminessTTF Nerd Font:size=11" }; /* Ubuntu Nerd Font*/
+static const char dmenufont[]       = "TerminessTTF Nerd Font:size=11";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -39,7 +39,7 @@ static char *colors[][3] = {
 },  */
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "", "", "", "" };
+static const char *tags[] = { " ", " ", " ", " ", " ", "", "", " " };
 
 
 static const Rule rules[] = {
